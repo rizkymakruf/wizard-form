3bb240918bae270cd3628aa799b2774de62d118e
rizkymakruf <armppy8@gmail.com>
2022-11-18 18:27:37 +0700

    wizard-step

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,88 @@ const Home = () => {
     <>
       <div className="w-full">
         <Navigasi />
+        <div className="w-full relative">
+          <div className="flex overflow-hidden  md:overflow-hidden noscroll overflow-x-scroll gap-5 p-5">
+            <div className="flex-none drop-shadow-md w-56 h-16 rounded-lg bg-white">
+              <div className="w-full flex">
+                <div className="flex">
+                  <div className="w-2 rounded-tl-lg rounded-bl-lg bg-blue-800 h-16"></div>
+                  <div className="flex justify-center items-center w-20 h-16 bg-blue-100">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-10 h-10 text-blue-800"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                      />
+                    </svg>
+                  </div>
+                  <div className="w-32 flex justify-center items-center text-gray-500 text-sm">
+                    Formulir Klaim
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex-none drop-shadow-md w-56 h-16 rounded-lg bg-white">
+              <div className="w-full flex">
+                <div className="flex">
+                  <div className="w-2 rounded-tl-lg rounded-bl-lg bg-blue-800 h-16"></div>
+                  <div className="flex justify-center items-center w-20 h-16 bg-blue-100">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-10 h-10 text-blue-800"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                      />
+                    </svg>
+                  </div>
+                  <div className="w-32 flex justify-center items-center text-gray-500 text-sm">
+                    Formulir Klaim
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex-none drop-shadow-md w-56 h-16 rounded-lg bg-white">
+              <div className="w-full flex">
+                <div className="flex">
+                  <div className="w-2 rounded-tl-lg rounded-bl-lg bg-blue-800 h-16"></div>
+                  <div className="flex justify-center items-center w-20 h-16 bg-blue-100">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-10 h-10 text-blue-800"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                      />
+                    </svg>
+                  </div>
+                  <div className="w-32 flex justify-center items-center text-gray-500 text-sm">
+                    Formulir Klaim
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
